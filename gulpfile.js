eb6e750d68df0ca7f148ee71d7648f7e07d78a28
Polina Nenchev <polina.mihova@yahoo.com>
2016-03-25 07:23:47 -0700

    Add Angular2 skeleton and update gulpfile by removing comments and notes in file for cleaner look

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,8 @@
 
 var gulp = require('gulp');
 
-// used for concatenating/minifying bower files and other js/css
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
-// used for pulling in bower files.
 var lib = require('bower-files')({
   "overrides":{
     "bootstrap" : {
@@ -18,39 +16,23 @@ var lib = require('bower-files')({
   }
 });
 
-// used for build and clean tasks.
 var utilities = require('gulp-util');
 var buildProduction = utilities.env.production;
 var del = require('del');
-
-// set up server with watchers and run typescript compiler in the shell.
 var browserSync = require('browser-sync').create();
 var shell = require('gulp-shell');
-
-// sass dependencies.
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 
-
-
 ////////////////////// TYPESCRIPT //////////////////////
 
-// clean task
 gulp.task('tsClean', function(){
   return del(['app/*.js', 'app/*.js.map']);
 });
 
-// clean and then compile once. To be called from server and global build.
 gulp.task('ts', ['tsClean'], shell.task([
   'tsc'
 ]));
-
-////////////////////// BOWER //////////////////////
-// when adding a new bower depndency:
-// stop the server
-// always use the `bower install --save` flag.
-// run `gulp bower` to build vendor files
-// restart server.
 
 gulp.task('jsBowerClean', function(){
   return del(['./build/js/vendor.min.js']);
@@ -75,8 +57,6 @@ gulp.task('cssBower', ['cssBowerClean'], function() {
 
 gulp.task('bower', ['jsBower', 'cssBower']);
 
-////////////////////// SASS //////////////////////
-
 gulp.task('sassBuild', function() {
   return gulp.src('resources/styles/*')
     .pipe(sourcemaps.init())
@@ -85,8 +65,6 @@ gulp.task('sassBuild', function() {
     .pipe(gulp.dest('./build/css'));
 });
 
-////////////////////// SERVER //////////////////////
-
 gulp.task('serve', function() {
   browserSync.init({
     server: {
@@ -94,10 +72,10 @@ gulp.task('serve', function() {
       index: "index.html"
     }
   });
-  gulp.watch(['resources/js/*.js'], ['jsBuild']); // vanilla js changes, reload.
-  gulp.watch(['*.html'], ['htmlBuild']); // html changes, reload.
-  gulp.watch(['resources/styles/*.css', 'resources/styles/*.scss'], ['cssBuild']); // css or sass changes, concatenate all css/sass, build, reload.
-  gulp.watch(['app/*.ts'], ['tsBuild']); // typescript files change, compile then reload.
+  gulp.watch(['resources/js/*.js'], ['jsBuild']);
+  gulp.watch(['*.html'], ['htmlBuild']);
+  gulp.watch(['resources/styles/*.css', 'resources/styles/*.scss'], ['cssBuild']);
+  gulp.watch(['app/*.ts'], ['tsBuild']);
 });
 
 gulp.task('jsBuild', function(){
@@ -116,27 +94,7 @@ gulp.task('tsBuild', ['ts'], function(){
   browserSync.reload();
 });
 
-////////////////////// GLOBAL BUILD TASK //////////////////////
-
-// global build task with individual clean tasks as dependencies.
 gulp.task('build', ['ts'], function(){
-  // we can use the buildProduction environment variable here later.
   gulp.start('bower');
   gulp.start('sassBuild');
 });
-
-////////////////////// SETUP NOTES //////////////////////
-
-/*
-- clone repo
-- npm install
-- bower install
-- install globals if needed (gulp, bower, sass, typescript, typescript packages.)
-  - npm install gulp -g
-  - npm install bower -g
-  - gem install sass
-  - npm install typescript -g
-  - apm install atom-typescript
-- gulp build
-- gulp serve
-*/
