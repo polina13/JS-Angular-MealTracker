42ee0c4ab905eeec59ba7cb8a71f9823444d6cf0
Polina Nenchev <polina.mihova@yahoo.com>
2016-03-25 08:35:45 -0700

    Add meal.model file and app.component file

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,15 +1,29 @@
 import { Component } from 'angular2/core';
+// import { MealListComponent } from './meal-list.component';
+// import { NewMealComponent } from './new-meal.component';
+
+import { Meal } from './meal.model';
 
 @Component ({
   selector: 'my-app',
+  // directives: [MealListComponent, NewMealComponent],
   template:`
   <div class="container">
-    <h1>Skeleton Angular2 App!</h1>
+    <h1>Meal Tracker!</h1>
   </div>
 
   `
 })
 
 export class AppComponent {
-
+  public meals: Meal[];
+  constructor(){
+    this.meals = [
+      new Meal("Chicken Gyro", "Grilled Cut it into pieces chicken, tomatoes, onion, tsaziki sauce wrapped with pita bread", 800),
+      new Meal("Greek Salad", "Tomatoes, cucumbers, and fresh onion, drizzled with vinegar and olive oil", 270)
+    ];
+  }
+  mealWasSelected(clickedMeal: Meal): void {
+    console.log(clickedMeal);
+  }
 }
